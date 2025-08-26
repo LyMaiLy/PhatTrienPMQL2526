877a28e5c473886d0e93c6ec5a303665134cf08d
LyMaiLy <lyymaii029@gmail.com>
2025-08-26 17:01:11 +0700

    mvc

--- DemoMvc/obj/Debug/net9.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net9.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f39680dcbe012a7172354dc382ac49f1ac62c5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47d31ef7191405fc10d6279854a9092e5949bf58")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
